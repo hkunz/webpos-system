c7ffe210288aab7391e048a27ad3a16d6e6b4c4f
Harry Kunz <harry.kunz0@gmail.com>
2019-11-30 14:19:55 +0800

    more efficient query

--- service/php/get-search-results.php
+++ service/php/get-search-results.php
@@ -4,13 +4,30 @@ include "../../php/db.php";
 if (isset($_POST['search'])) {
 	$Name = str_replace(" ", "%", $_POST['search']);
    	$Query = "
-		SELECT * FROM view_items
-		WHERE sell_price_latest > 0 AND (item_description LIKE '%$Name%'
-		OR bar_code LIKE '%$Name%'
-		OR general_name LIKE '%$Name%'
-		OR brand_name LIKE '%$Name%'  
-		OR category LIKE '%$Name%')
-		ORDER BY item_description DESC
+		SELECT
+			ii.item_id,
+			ii.bar_code,
+			ii.unit,
+			ii.item_description,
+			ii.general_name,
+			ii.brand_name,
+			ii.category,
+			ii.supplier_name,
+			pp.sell_price,
+			ss.stock
+		FROM items ii
+		LEFT JOIN items_stock ss
+			ON ss.item_id=ii.item_id
+		LEFT JOIN view_items_prices_latest pp
+			ON pp.item_id=ii.item_id
+		WHERE pp.sell_price > 0 AND (
+			ii.item_description LIKE '%$Name%' OR
+			ii.bar_code LIKE '%$Name%' OR
+			ii.general_name LIKE '%$Name%' OR
+			ii.brand_name LIKE '%$Name%' OR
+			ii.category LIKE '%$Name%'
+		)
+		ORDER BY ii.item_description DESC
 		LIMIT 20;"
 	;
 
@@ -21,7 +38,7 @@ if (isset($_POST['search'])) {
 		$code = (empty($code) ? "XXXXXXXXXXXXX" : $code);
 		$unit = $Result['unit'];
 		$description = $Result['item_description'];
-		$price = $Result['sell_price_latest'];
+		$price = $Result['sell_price'];
 		$id = $Result['item_id'];
 		$category = $Result['category'];
 		$general_name = $Result['general_name'];

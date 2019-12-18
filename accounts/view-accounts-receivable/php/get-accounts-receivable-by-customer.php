2ffe870d558242656f8ae9876b965340aa7a6ecf
Harry Kunz <harry.kunz0@gmail.com>
2019-12-18 17:58:19 +0800

    make nice table & scrollbar

--- accounts/view-accounts-receivable/php/get-accounts-receivable-by-customer.php
+++ accounts/view-accounts-receivable/php/get-accounts-receivable-by-customer.php
@@ -6,19 +6,24 @@ $currency = $_POST['currency'];
 $customer = $_POST['customer'];
 
 function customers_table_content($con) {
+	$customer_w = "350px";
+	$date_w = "195px";
+	$total_w = "100px";
+	$payment_w = "100px";
+	$vscroll_w2 = "6px";
 	$customer_field = 'customer';
 	$date_field = 'last_update';
 	$payment_field = 'payment';
 	$total_field = 'grand_total';
 	$query = "SELECT `$customer_field`,MAX(`date`) `$date_field`,SUM(`payment`) `$payment_field`,SUM(`grand_total`) `$total_field` FROM `items_transactions` WHERE `type`='SALE' AND `payment` < `grand_total` GROUP BY `customer` ORDER BY `customer` ASC;";
 	$result = $con->query($query);
-	$table = "<table id='customer_table' class='common-table common-table-scroll' cellspacing='0' cellpadding='0'><thead class='scroll'><tr><th>Customer</th><th width='150px' nowrap>Last Update</th><th nowrap>Grand Total</th><th nowrap>Payment</th></tr></thead><tbody class='scroll'>";
+	$table = "<table id='customer_table' class='common-table common-table-scroll' cellspacing='0' cellpadding='0'><thead class='scroll'><tr><th style='width:$customer_w;'>Customer</th><th style='width:$date_w;' nowrap>Last Update</th><th style='width:$total_w;text-align:right;' nowrap>Grand Total</th><th style='width:$payment_w;text-align:right;' nowrap>Payment</th><th style='padding-left:$vscroll_w2;padding-right:$vscroll_w2;'></th></tr></thead><tbody class='scroll'>";
 	while($row = $result->fetch_array()) {
 		$customer = $row[$customer_field];
 		$date = $row[$date_field];
 		$payment = $row[$payment_field];
 		$total = $row[$total_field];
-		$table .= "<tr><td width='100%'>$customer</td><td nowrap>$date</td><td align='right' nowrap><span style='margin-right:2px;'>$currency</span>$total</td><td align='right' nowrap><span style='margin-right:2px;'$currency</span>$payment</td></tr>";
+		$table .= "<tr><td style='width:$customer_w;'>$customer</td><td style='width:$date_w;' nowrap>$date</td><td style='width:$total_w;text-align:right;' nowrap><span style='margin-right:2px;'>$currency</span>$total</td><td style='width:$payment_w;text-align:right;' nowrap><span style='margin-right:2px;'$currency</span>$payment</td></tr>";
 	}
 	$table .= "</tbody></table>";
 	return $table;

926fd3f22359aba799f10a8bd5a616d665d892b2
Harry Kunz <harry.kunz0@gmail.com>
2019-12-23 18:20:14 +0800

    check if mobile

--- welcome.php
+++ welcome.php
@@ -10,11 +10,9 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     header("location: login.php");
     exit;
 }
-#ob_start();
-#require_once('php/check-detect-mobile-device.php');
-#$is_mobile = ob_get_clean();
-#echo "'" . $is_mobile . "'";
-#exit;
+ob_start();
+require_once('php/check-detect-mobile-device.php');
+$is_mobile = ob_get_clean() === '1';
 require_once('php/navigation-bar.php');
 $username = htmlspecialchars($_SESSION["username"]);
 ?>
@@ -31,11 +29,14 @@ $username = htmlspecialchars($_SESSION["username"]);
   <script type="text/javascript" src="js/main.js"></script>
 </head>
 <body class="body" style='overflow:hidden'>
-  <?php echo " 
-  <div class='navbar' style='padding:12px;padding-left:16px;'><label class='header-caption'><script type='text/javascript'>document.write(Utils.getStoreHeading());</script></label></div>
-  "; ?>
+  <?php echo $is_mobile ? "<div class='navbar' style='padding:12px;padding-left:16px;'><label class='header-caption'><script type='text/javascript'>document.write(Utils.getStoreHeading());</script></label></div>
+  " : $navbar_content ?>
   <div class="container-wrapper" style='padding-bottom:0px;margin-bottom:50px;'>
   <div class="container-left" style='width:100%;max-width:500px;margin-bottom:0px;'>
+    <div class='store-heading' <?php echo $is_mobile ? "style='display:none'" : ""; ?>>
+      <label class="heading"><script type="text/javascript">document.write(Utils.getStoreHeading());</script></label>                                                              | <label class="heading-sub"><script type="text/javascript">document.write(Utils.getStoreSubHeading());</script></label>
+      <hr class="division">
+    </div>
     <h1>Welcome <?php echo $username; ?></h1>
     <div>
       <img class="welcome-image" src="imgs/makoy.jpg" style='min-width:100px;max-width:330px;width:100%;'>

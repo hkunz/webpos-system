6a0aeafab71877a43490539f67a250531867bc62
Harry Kunz <harry.kunz0@gmail.com>
2019-12-23 18:09:00 +0800

    fix additional echo space bug

--- php/detect-mobile-device.php
+++ php/detect-mobile-device.php
@@ -4,6 +4,6 @@ $root = isset($_SESSION['root']) ? $_SESSION['root'] : '';
 $href_root = isset($_SESSION['href_root']) ? $_SESSION['href_root'] : '';
 ob_start();
 require_once("${root}php/check-detect-mobile-device.php");
-$is_mobile = rtrim(ob_get_clean());
+$is_mobile = ob_get_clean();
 echo ($is_mobile === '1') ? "${href_root}css/main-styles-mobile.css" : "";
 ?>

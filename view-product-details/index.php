a5529187b1e4bb839b52efbdbc7802f522604bd6
Harry Kunz <harry.kunz0@gmail.com>
2019-12-29 13:01:39 +0800

    mobile view for view product details

--- view-product-details/index.php
+++ view-product-details/index.php
@@ -6,17 +6,22 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 }
 $href_root = $_SESSION['href_root'];
 $root = $_SESSION['root'];
-require_once("${root}php/navigation-bar.php");
+
+ob_start();
+require_once("${root}php/check-detect-mobile-device.php");
+$ismobile = ob_get_clean() === '1';
+require_once("${root}php/" . ($ismobile ? "mini-navigation-bar" : "navigation-bar") . ".php");
 ?>
 <!DOCTYPE html>
 <html lang="en">
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Klebby's Store</title>
+  <title>View Product Details</title>
   <link type="text/css" rel="stylesheet" href="<?php echo $href_root; ?>css/main-styles.css">
   <link type="text/css" rel="stylesheet" href="<?php echo $href_root; ?>css/navigation-bar.css">
   <link type="text/css" rel="stylesheet" href="<?php echo $href_root; ?>css/awesomplete.css">
   <link type="text/css" rel="stylesheet" href="<?php echo $href_root; ?>css/common-table.css">
+  <link type="text/css" rel="stylesheet" href="<?php echo ($ismobile ? $href_root . 'css/main-styles-mobile.css' : '') ?>">
   <script type="text/javascript" src="<?php echo $href_root; ?>js/libs/jquery-3.4.1.min.js"></script>
   <script type="text/javascript" src="<?php echo $href_root; ?>js/libs/awesomplete.js"></script>
   <script type="text/javascript" src="<?php echo $href_root; ?>js/AwesompleteInputHandler.js"></script>
@@ -33,16 +38,18 @@ require_once("${root}php/navigation-bar.php");
   <?php echo $navbar_content; ?>
   <div class="container-wrapper">
     <div class="container-left">
-      <label class="heading"><script type="text/javascript">document.write(Utils.getStoreHeading());</script></label>                                                              | <label class="heading-sub"><script type="text/javascript">document.write(Utils.getStoreSubHeading());</script></label>
-      <hr class="division">
+      <div class='store-heading'>
+        <label class="heading"><script type="text/javascript">document.write(Utils.getStoreHeading());</script></label> | <label class="heading-sub"><script type="text/javascript">document.write(Utils.getStoreSubHeading());</script></label>
+        <hr class="division">
+      </div>
       <div style="margin-left:2px;margin-bottom:3px;">
         <label class="drop-shadow" style="font-weight:bold;">VIEW PRODUCT DETAILS</label>
       </div>
       <div class="search_item">
         <input type="text" class="awesomplete" id="search_item_input" placeholder="Type here to search product ..." spellcheck="false" />
       </div>
-      <div class="common-table-wrapper">
-      <table class="common-table" cellspacing="0" cellpadding="0">
+      <div class="common-table-wrapper" style='overflow-y:auto;max-height:100%;'>
+      <table class="common-table" cellspacing="0" cellpadding="0" style='height:100%;'>
         <thead>
         <tr>
           <th id="product_description" colspan='2'>Description</th>

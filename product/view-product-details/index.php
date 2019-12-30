13cdb1a75d566b39506a8cb79d6ccc66bc0586f9
Harry Kunz <harry.kunz0@gmail.com>
2019-12-30 13:21:35 +0800

    make nice view product details mobile

--- product/view-product-details/index.php
+++ product/view-product-details/index.php
@@ -22,6 +22,7 @@ require_once("${root}php/" . ($ismobile ? "mini-navigation-bar" : "navigation-ba
   <link type="text/css" rel="stylesheet" href="<?php echo $href_root; ?>css/awesomplete.css">
   <link type="text/css" rel="stylesheet" href="<?php echo $href_root; ?>css/common-table.css">
   <link type="text/css" rel="stylesheet" href="<?php echo ($ismobile ? $href_root . 'css/main-styles-mobile.css' : '') ?>">
+  <link type="text/css" rel="stylesheet" href="css/style.css">
   <script type="text/javascript" src="<?php echo $href_root; ?>js/libs/jquery-3.4.1.min.js"></script>
   <script type="text/javascript" src="<?php echo $href_root; ?>js/libs/awesomplete.js"></script>
   <script type="text/javascript" src="<?php echo $href_root; ?>js/AwesompleteInputHandler.js"></script>
@@ -48,59 +49,66 @@ require_once("${root}php/" . ($ismobile ? "mini-navigation-bar" : "navigation-ba
       <div class="search_item">
         <input type="text" class="awesomplete" id="search_item_input" placeholder="Type here to search product ..." spellcheck="false" />
       </div>
-      <div class="common-table-wrapper" style='overflow-y:auto;max-height:100%;'>
-      <table class="common-table" cellspacing="0" cellpadding="0" style='height:100%;'>
-        <thead>
+      <div class="common-table-wrapper" style='height:100%;max-height:100%;display:flex;flex-flow:column;'>
+      <div>
+      <table class="common-table" cellspacing="0" cellpadding="0" style=''>
+        <thead style='display:table;'>
         <tr>
           <th id="product_description" colspan='2'>Description</th>
         </tr>
-        </thead><tbody>
+        </thead>
+      </table>
+      </div>
+      <div style='overflow-y:auto;flex:1;height:100%;'>
+      <table class="common-table" cellspacing="0" cellpadding="0" style='height:100%;'>
+        <tbody style='display:flex;flex-flow:column;height:100%;overflow:hidden'>
         <tr>
-          <td width="150px"><label class="standard-label">Product ID</label></td>
-          <td><label id="product_id" class="drop-shadow"></label>
+          <td class='td-col'><label class="standard-label">Product ID</label></td>
+          <td class='td2-col'><label id="product_id" class="drop-shadow"></label>
         </tr>
         <tr>
-          <td><label class="standard-label">Bar Code</label></td>
-          <td><label id="product_barcode" class="drop-shadow"></label>
+          <td class='td-col'><label class="standard-label">Bar Code</label></td>
+          <td class='td2-col'><label id="product_barcode" class="drop-shadow"></label>
         </tr>
         <tr>
-          <td><label class="standard-label">Content Count</label></td>
-          <td><label id="content_count" class="drop-shadow"></label>
+          <td class='td-col'><label class="standard-label">Content Count</label></td>
+          <td class='td2-col'><label id="content_count" class="drop-shadow"></label>
         </tr>
         <tr>
-          <td><label class="standard-label">Stock</label></td>
-          <td><label id="stock" class="drop-shadow"></label>
+          <td class='td-col'><label class="standard-label">Stock</label></td>
+          <td class='td2-col'><label id="stock" class="drop-shadow"></label>
         </tr>
         <tr>
-          <td><label class="standard-label">Category</label></td>
-          <td><label id="product_category" class="drop-shadow"></label>
+          <td class='td-col'><label class="standard-label">Category</label></td>
+          <td class='td2-col'><label id="product_category" class="drop-shadow"></label>
         </tr>
         <tr>
-          <td><label class="standard-label">Unit Price</label></td>
-          <td><label id="unit_price" class="drop-shadow"></label>
+          <td class='td-col'><label class="standard-label">Unit Price</label></td>
+          <td class='td2-col'><label id="unit_price" class="drop-shadow"></label>
         </tr>
         <tr>
-          <td><label class="standard-label">Sell Price</label></td>
-          <td><label id="sell_price" class="drop-shadow"></label>
+          <td class='td-col'><label class="standard-label">Sell Price</label></td>
+          <td class='td2-col'><label id="sell_price" class="drop-shadow"></label>
         </tr>
         <tr>
-          <td><label class="standard-label">General Name</label></td>
-          <td><label id="general_name" class="drop-shadow"></label>
+          <td class='td-col'><label class="standard-label">General Name</label></td>
+          <td class='td2-col'><label id="general_name" class="drop-shadow"></label>
         </tr>
         <tr>
-          <td><label class="standard-label">Brand Name</label></td>
-          <td><label id="brand_name" class="drop-shadow"></label>
+          <td class='td-col'><label class="standard-label">Brand Name</label></td>
+          <td class='td2-col'><label id="brand_name" class="drop-shadow"></label>
         </tr>
         <tr>
-          <td><label class="standard-label">Supplier</label></td>
-          <td><label id="product_supplier" class="drop-shadow"></label>
+          <td class='td-col'><label class="standard-label">Supplier</label></td>
+          <td class='td2-col'><label id="product_supplier" class="drop-shadow"></label>
         </tr>
         <tr>
-          <td><label class="standard-label">Manufacturer</label></td>
-          <td><label id="product_manufacturer" class="drop-shadow"></label>
+          <td class='td-col'><label class="standard-label">Manufacturer</label></td>
+          <td class='td2-col'><label id="product_manufacturer" class="drop-shadow"></label>
         </tr>
         </tbody>
         </table>
+        </div>
         </div>
       </div>
     </div>

b1a00aaaa00fcedd575c38489e578936620cad6a
Harry Kunz <harry.kunz0@gmail.com>
2020-01-08 21:18:07 +0800

    DateUtils.js cleanup

--- js/DateUtils.js
+++ js/DateUtils.js
@@ -7,9 +7,9 @@ class DateUtils {
 		const month = '' + (d.getMonth() + 1);
 		const day = '' + d.getDate();
 		const year = d.getFullYear();
-		const h = DateUtils.lpad(d.getHours(), '0', 2);
-		const m = DateUtils.lpad(d.getMinutes(), '0', 2);
-		const s = DateUtils.lpad(d.getSeconds(), '0', 2);
+		const h = DateUtils.lpad2(d.getHours());
+		const m = DateUtils.lpad2(d.getMinutes());
+		const s = DateUtils.lpad2(d.getSeconds());
 		const time = h + ":" + m + ":" + s;
 		return year + '-' + month + '-' + day + ' ' + (h + ":" + m + ":" + s);
 	}
@@ -22,10 +22,16 @@ class DateUtils {
 		return (str + text).slice(-length);
 	}
 
+	static lpad2(text) {
+		return DateUtils.lpad(text, '0', 2);
+	}
+
 	// returns 'yyyy-MM-dd hh:mm:ss' formatted string
 	static getTimestampNowUTC() {
 		let m = new Date();
-		return m.getUTCFullYear() +"-"+ (m.getUTCMonth()+1) +"-"+ m.getUTCDate() + " " + m.getUTCHours() + ":" + m.getUTCMinutes() + ":" + m.getUTCSeconds();
+		let date = m.getUTCFullYear() + "-" + DateUtils.lpad2(m.getUTCMonth() + 1) + "-" + DateUtils.lpad2(m.getUTCDate());
+		let time = DateUtils.lpad2(m.getUTCHours()) + ":" + DateUtils.lpad2(m.getUTCMinutes()) + ":" + DateUtils.lpad2(m.getUTCSeconds());
+		return date + " " + time;
 	}
 
 	static getTimestampNow() {
@@ -38,11 +44,11 @@ class DateUtils {
 	}
 
 	static getDateString(date) {
-		return date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate();
+		return date.getFullYear() + '-' + DateUtils.lpad2(date.getMonth() + 1) + '-' + DateUtils.lpad2(date.getDate());
 	}
 
 	static getDateTimeString(date) {
-		return DateUtils.getDateString(date) + ' ' + date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds();
+		return DateUtils.getDateString(date) + ' ' + DateUtils.lpad2(date.getHours()) + ':' + DateUtils.lpad2(date.getMinutes()) + ':' + DateUtils.lpad2(date.getSeconds());
 	}
 
 	static getTimestampPreviousMonthStart() {
@@ -79,23 +85,11 @@ class DateUtils {
 		return DateUtils.getDateStringEnd(d);
 	}
 
-	static getTimestampMonthStart(month) {
-		let m = new Date();
-		return m.getFullYear() + "-" + ("0" + month).slice(-2) + "-01 00:00:00";
-	}
-
 	static getDateStringStart(date) {
 		return DateUtils.getDateString(date) + ' 00:00:00';
 	}
 
 	static getDateStringEnd(date) {
 		return DateUtils.getDateString(date) + ' 23:59:59';
-	}
-
-	//month between 1-12; 1=January 12=December
-	static getTimestampMonthEnd(month) {
-		let m = new Date();
-                let d = new Date(m.getFullYear(), month, 0);
-                return DateUtils.getDateString(d) + " 23:59:59";
 	}
 }

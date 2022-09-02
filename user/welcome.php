511d98d34f8fb19d43ced01cdd9d0b79503cb4b1
Harry Kunz <harry.kunz0@gmail.com>
2022-09-02 15:32:21 +0800

    last changes 2020

--- user/welcome.php
+++ user/welcome.php
@@ -26,14 +26,14 @@ $username = htmlspecialchars($_SESSION["username"]);
   <?php echo $ismobile ? "<div class='navbar' style='padding:12px;padding-left:16px;'><label class='header-caption'><script type='text/javascript'>document.write(Utils.getStoreHeading());</script></label></div>
   " : $navbar_content ?>
   <div class="container-wrapper" style='margin-bottom:30px;'>
-  <div class="container-left" style='max-height:<?php echo ($ismobile) ? "100%" : "450px" ?>px;max-width:<?php echo ($ismobile) ? "100%" : "500px" ?>;margin-bottom:15px;'>
+  <div class="container-left" style='max-height:<?php echo ($ismobile) ? "100%" : "450px" ?>px;max-width:<?php echo ($ismobile) ? "100%" : "100%" ?>;margin-bottom:15px;'>
     <div class='store-heading' <?php echo $ismobile ? "style='display:none'" : ""; ?>>
       <label class="heading"><script type="text/javascript">document.write(Utils.getStoreHeading());</script></label>                                                              | <label class="heading-sub"><script type="text/javascript">document.write(Utils.getStoreSubHeading());</script></label>
       <hr class="division">
     </div>
     <h1>Welcome <?php echo $username; ?></h1>
     <div>
-      <img class="welcome-image" src="<?php echo $href_root; ?>/assets/imgs/makoy.jpg" style='min-width:100px;max-width:330px;width:100%;'>
+      <img class="welcome-image" src="<?php echo $href_root; ?>/assets/imgs/logo.png" style='min-width:200px;max-width:100%;width:100%;'>
     </div>
   </div>
   </div>
